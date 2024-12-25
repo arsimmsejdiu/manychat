83218cc1398925b9e78724241dbd0a8677a43ac0
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-12-25 14:33:37 +0100

    minor fixes to the app and changing the font of the app

---
+++ src/components/global/automations/post/node.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+// import { useQueryAutomation } from "@/hooks/user-queries";
+import { InstagramBlue, Warning } from "@/icons";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  id: string;
+};
+
+const Node = ({ id }: Props) => {
+    // const { data } = useQueryAutomation(id);
+
+    
+};
