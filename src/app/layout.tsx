2637c74fadd34b5013e3b0d1a02719c825ba1ae5
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-12-25 17:06:26 +0100

    Added Theme, Redux and React Query Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Toaster } from "sonner";
+import ReactQueryProvider from "@/providers/react-query-provider";
+import ReduxProvider from "@/providers/redux-provider";
+
 import "./globals.css";
 
-
-const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
+const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Slide',
-  description: 'Automate DMs and comments on instagram',
-}
+  title: "Slide",
+  description: "Automate DMs and comments on instagram",
+};
 
 export default function RootLayout({
   children,
@@ -17,7 +21,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jakarta.className}>{children}</body>
+      <body className={jakarta.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          <ReduxProvider>
+            <ReactQueryProvider>{children} </ReactQueryProvider>
+          </ReduxProvider>
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
