c13d534e7771cbf077755a27460e267a8d936a70
arsimsejdiu <arsim.sejdiu@bik-air.com>
2024-12-26 01:34:56 +0100

    Finished with the settings page

--- src/components/global/billing/index.tsx
+++ src/components/global/billing/index.tsx
@@ -2,22 +2,15 @@
 
 import React from "react";
 import PaymentCard from "./payment-card";
-// import PaymentCard from './payment-card'
-// import { useQueryUser } from '@/hooks/user-queries'
+import { useQueryUser } from "@/hooks/user-queries";
 
 const Billing = () => {
-  // const { data } = useQueryUser();
+  const { data } = useQueryUser();
 
   return (
     <div className="flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container">
-      <PaymentCard
-        // current={data?.data?.subscription?.plan!} // TODO: in Payment Card remove the ? (optional) from current
-        label="PRO"
-      />
-      <PaymentCard
-        // current={data?.data?.subscription?.plan!}
-        label="FREE"
-      />
+      <PaymentCard current={data?.data?.subscription?.plan!} label="PRO" />
+      <PaymentCard current={data?.data?.subscription?.plan!} label="FREE" />
     </div>
   );
 };
